8a88afcc7db085affe9f7da7a577f0f27d3f1527
Piotr Skrobski <234796@edu.p.lodz.pl>
2024-04-03 20:15:37 +0200

    Add Category atom

---
+++ src/components/atoms/Category.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="category">
+    <span>&bull; {{ category }}</span>
+    <span class="number">({{ numberOfOffers }})</span>
+  </div>
+</template>
+
+<script>
+export default {
+// eslint-disable-next-line vue/multi-word-component-names
+  name: "Category",
+  props: {
+    category: {
+      type: String,
+      required: true,
+    },
+    numberOfOffers: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.category {
+  display: flex;
+  justify-content: space-between;
+  margin-left: min(5%, 10px);
+}
+
+.category span {
+  margin-right: min(5%, 10px);
+}
+</style>

15621d3338c30d7d7746912fcc0f05d3c4081a5e
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2024-04-27 17:39:03 +0200

    Format offer creation time

--- src/components/organisms/Offer.vue
+++ src/components/organisms/Offer.vue
@@ -85,11 +85,11 @@ export default {
 
       this.offerTitle = response.data.title;
       this.offerDescription = response.data.description;
-      this.submittedOn = response.data.creationDate;
-      this.location = response.data.city + ' (' + response.data.distance + ' od ciebie)'; // TODO: fix distance
+      this.submittedOn = response.data.creationDate.substring(0, 10);
+      this.location = response.data.city + ', ' + response.data.street + ' (' + response.data.distance + ' od ciebie)';
       this.condition = response.data.condition;
-      // this.amountOfStars = response.data.ownerRating;
-      // this.amountOfRatings = response.data.ownerReviewCount;
+      this.amountOfStars = response.data.ownerRating;
+      this.amountOfRatings = response.data.ownerReviewCount;
       this.userFirstName = response.data.ownerName;
       this.userSurname = response.data.ownerSurname;
     }).catch(error => {

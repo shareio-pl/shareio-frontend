fc70e8170c8aab7c07ef07152b8709edf28d236d
Rafal Cybula <242370@edu.p.lodz.pl>
2024-05-30 23:52:49 +0200

    Add AI Popup

--- src/components/organisms/FormOffer.vue
+++ src/components/organisms/FormOffer.vue
@@ -1,47 +1,50 @@
 <template>
   <form id="form-offer" @submit.prevent="submitForm">
+    <AIPopUp/>
     <div id="form-offer-title">
       <FieldInput v-model="offerTitle" placeholder="Tytuł" label="Tytuł"
-        :error="{ active: v$.offerTitle.$error && v$.offerTitle.$dirty, message: '' }" />
+                  :error="{ active: v$.offerTitle.$error && v$.offerTitle.$dirty, message: '' }"/>
     </div>
     <div class="form-offer-flex">
       <div id="form-offer-location">
         <FieldInput v-model="offerCity" placeholder="Miasto" label="Miasto"
-          :error="{ active: v$.offerCity.$error && v$.offerCity.$dirty, message: '' }" />
+                    :error="{ active: v$.offerCity.$error && v$.offerCity.$dirty, message: '' }"/>
         <FieldInput v-model="offerStreet" placeholder="Ulica" label="Ulica"
-          :error="{ active: v$.offerStreet.$error && v$.offerStreet.$dirty, message: '' }" />
+                    :error="{ active: v$.offerStreet.$error && v$.offerStreet.$dirty, message: '' }"/>
         <FieldInput v-model="offerHomeNumber" placeholder="Numer domu" label="Numer domu"
-          :error="{ active: v$.offerHomeNumber.$error && v$.offerHomeNumber.$dirty, message: '' }" />
+                    :error="{ active: v$.offerHomeNumber.$error && v$.offerHomeNumber.$dirty, message: '' }"/>
       </div>
       <div id="form-offer-options">
-        <DropdownSelect :options="categories" type="Category" placeholder="Wybierz kategorię" />
-        <DropdownSelect :options="states" type="State" placeholder="Wybierz stan" />
+        <DropdownSelect :options="categories" type="Category" placeholder="Wybierz kategorię"/>
+        <DropdownSelect :options="states" type="State" placeholder="Wybierz stan"/>
       </div>
     </div>
     <div id="form-offer-description" class="position-relative">
       <FieldTextBox v-model="offerDescription" placeholder="Opis" label="Opis przedmiotu" style="width:200%;"
-        :disabled="isAIDescriptionLoading"
-        :error="{ active: v$.offerDescription.$error && v$.offerDescription.$dirty, message: '' }" />
+                    :disabled="isAIDescriptionLoading"
+                    :error="{ active: v$.offerDescription.$error && v$.offerDescription.$dirty, message: '' }"/>
       <div class="spinner" v-if="isAIDescriptionLoading"></div>
     </div>
   </form>
 </template>
 
 <script>
-import { GATEWAY_ADDRESS } from "../../../public/Consts";
+import {GATEWAY_ADDRESS} from "../../../public/Consts";
 
-import { required, minLength, maxLength } from '@vuelidate/validators'
-import { useVuelidate } from '@vuelidate/core';
+import {required, minLength, maxLength} from '@vuelidate/validators'
+import {useVuelidate} from '@vuelidate/core';
 
 import axios from 'axios';
 
 import FieldInput from "../atoms/FieldInput.vue";
 import FieldTextBox from "../atoms/FieldTextBox.vue";
 import DropdownSelect from "../atoms/DropdownSelect.vue";
+import AIPopUp from "@/components/organisms/AI-pop-up.vue";
 
 export default {
   name: 'OfferForm',
   components: {
+    AIPopUp,
     FieldInput,
     FieldTextBox,
     DropdownSelect,
@@ -83,23 +86,24 @@ export default {
       condition: '',
       categoryDisplayName: '',
       conditionDisplayName: '',
+      additionalData: '',
       isAIDescriptionLoading: false,
     };
   },
   validations() {
     return {
-      offerTitle: { required, minLength: minLength(5) },
-      offerDescription: { required, minLength: minLength(20), maxLength: maxLength(255) },
-      offerCity: { required, minLength: minLength(3) },
-      offerStreet: { required, minLength: minLength(3) },
-      offerHomeNumber: { required, minLength: minLength(1) },
-      category: { required },
-      condition: { required },
+      offerTitle: {required, minLength: minLength(5)},
+      offerDescription: {required, minLength: minLength(20), maxLength: maxLength(255)},
+      offerCity: {required, minLength: minLength(3)},
+      offerStreet: {required, minLength: minLength(3)},
+      offerHomeNumber: {required, minLength: minLength(1)},
+      category: {required},
+      condition: {required},
     }
   },
   setup() {
     const v$ = useVuelidate()
-    return { v$ }
+    return {v$}
   },
   methods: {
     submitForm() {
@@ -108,29 +112,12 @@ export default {
         return
       }
     },
-    async generateDescriptionByAI() {
+    async checkIfCanGenerate() {
       if (this.offerTitle === '' || this.condition === '' || this.category === '') {
         console.log("Offer title, condition or category is empty");
-        this.emitter.emit('error', { error: 'Tytuł, stan lub kategoria jest pusta' });
-      }
-      else {
-        this.isAIDescriptionLoading = true;
-        axios.get(GATEWAY_ADDRESS + `/offer/generateDescription`, {
-          params: {
-            title: this.offerTitle, condition: this.conditionDisplayName,
-            category: this.categoryDisplayName
-          }
-        })
-          .then((response) => {
-            console.log("Description from AI: ", response.data);
-            this.offerDescription = response.data;
-            this.isAIDescriptionLoading = false;
-          })
-          .catch((error) => {
-            console.log(error);
-            this.emitter.emit('axiosError', { error: error.response.status });
-            this.isAIDescriptionLoading = false;
-          })
+        this.emitter.emit('error', {error: 'Tytuł, stan lub kategoria jest pusta!'});
+      } else {
+        this.emitter.emit('generateDescription');
       }
     },
     async getFormData() {
@@ -141,31 +128,24 @@ export default {
         let errorMessage = '';
         if (this.v$.offerTitle.$error) {
           errorMessage += this.offerTitleError + ', ';
-        }
-        else if (this.v$.offerCity.$error) {
+        } else if (this.v$.offerCity.$error) {
           errorMessage += this.offerCityError + ', ';
-        }
-        else if (this.v$.offerStreet.$error) {
+        } else if (this.v$.offerStreet.$error) {
           errorMessage += this.offerStreetError + ', ';
-        }
-        else if (this.v$.offerHomeNumber.$error) {
+        } else if (this.v$.offerHomeNumber.$error) {
           errorMessage += this.offerHomeNumberError + ', ';
-        }
-        else if (this.v$.offerDescription.$error) {
+        } else if (this.v$.offerDescription.$error) {
           errorMessage += this.offerDescriptionError + ', ';
-        }
-        else if (this.category === '') {
+        } else if (this.category === '') {
           errorMessage += 'Kategoria jest obowiązkowa, ';
-        }
-        else if (this.condition === '') {
+        } else if (this.condition === '') {
           errorMessage += 'Stan jest obowiązkowy, ';
         }
         errorMessage = errorMessage.slice(0, -2);
-        this.emitter.emit('error', { error: errorMessage });
+        this.emitter.emit('error', {error: errorMessage});
         console.log("Error w walidacji: ", errorMessage);
         return null;
-      }
-      else {
+      } else {
         return {
           offerTitle: this.offerTitle,
           offerDescription: this.offerDescription,
@@ -182,19 +162,63 @@ export default {
       if (data.type === 'Category') {
         this.category = data.option;
         this.categoryDisplayName = data.displayName;
-      }
-      else if (data.type === 'State') {
+      } else if (data.type === 'State') {
         this.condition = data.option;
         this.conditionDisplayName = data.displayName;
       }
     },
+    generateWithoutAdditionalData() {
+      this.isAIDescriptionLoading = true;
+      axios.get(GATEWAY_ADDRESS + `/offer/generateDescription`, {
+        params: {
+          title: this.offerTitle, condition: this.conditionDisplayName,
+          category: this.categoryDisplayName
+        }
+      }).then((response) => {
+        console.log("Description from AI: ", response.data);
+        this.offerDescription = response.data;
+        this.isAIDescriptionLoading = false;
+      }).catch((error) => {
+        console.log(error);
+        this.emitter.emit('axiosError', {error: error.response.status});
+        this.isAIDescriptionLoading = false;
+      })
+    },
+    generateWithAdditionalData() {
+      this.isAIDescriptionLoading = true;
+      axios.get(GATEWAY_ADDRESS + `/offer/generateDescription`, {
+        params: {
+          title: this.offerTitle,
+          condition: this.conditionDisplayName,
+          category: this.categoryDisplayName,
+          additionalData: this.additionalData
+        }
+      }).then((response) => {
+        console.log("Description from AI: ", response.data);
+        this.offerDescription = response.data;
+        this.isAIDescriptionLoading = false;
+      }).catch((error) => {
+        console.log(error);
+        this.emitter.emit('axiosError', {error: error.response.status});
+        this.isAIDescriptionLoading = false;
+      })
+    },
   },
   mounted() {
     this.emitter.on('dropdown-change', this.handleDropdownOptions);
+
+    this.emitter.on('addedData', (data) => {
+      if (data.additionalData === '') {
+        this.generateWithoutAdditionalData();
+      } else {
+        this.additionalData = data.additionalData;
+        this.generateWithAdditionalData();
+      }
+    })
   },
   watch: {
     generateDescription: function () {
-      this.generateDescriptionByAI();
+      this.checkIfCanGenerate();
     },
     'v$.offerTitle.$error'(newVal) {
       if (newVal) {
@@ -234,12 +258,11 @@ export default {
       }
     },
     'v$.offerDescription.$error'(newVal) {
-      // TODO: Fix this, rn it doesn't seem to work with ifs. 
+      // TODO: Fix this, rn it doesn't seem to work with ifs.
       if (newVal) {
         if (!this.v$.offerDescription.minLength.$model) {
           this.offerDescriptionError = `Opis musi mieć co najmniej ${this.v$.offerDescription.minLength.$params.min} znaków`;
-        }
-        else if (!this.v$.offerDescription.maxLength.$model) {
+        } else if (!this.v$.offerDescription.maxLength.$model) {
           this.offerDescriptionError = `Opis musi mieć co najwyżej ${this.v$.offerDescription.maxLength.$params.max} znaków`;
         }
       } else {

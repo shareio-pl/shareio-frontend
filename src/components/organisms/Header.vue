883c59844dfeebf7fd5b909786697d2804fe72ce
Piotr Skrobski <234796@edu.p.lodz.pl>
2024-05-31 19:52:59 +0200

    Restore arrow on menu

--- src/components/organisms/Header.vue
+++ src/components/organisms/Header.vue
@@ -4,38 +4,38 @@
       <div id="header_wrapper">
         <img src="../../assets/logo.png" alt="" @click="onLogoClick" class="logo">
         <div id="buttons">
-          <ButtonPrimary class="button" button-text="Ogłoszenia" @click="onOffersClick"/>
-          <ButtonPrimary class="button" button-text="Mapa" @click="onMapClick"/>
-          <ButtonPrimary class="button" button-text="Nowa oferta" @click="onNewOfferClick"/>
-          <ButtonPrimary class="button" button-text="O nas" @click="onAboutUsClick"/>
+          <ButtonPrimary class="button" button-text="Ogłoszenia" @click="onOffersClick" />
+          <ButtonPrimary class="button" button-text="Mapa" @click="onMapClick" />
+          <ButtonPrimary class="button" button-text="Nowa oferta" @click="onNewOfferClick" />
+          <ButtonPrimary class="button" button-text="O nas" @click="onAboutUsClick" />
         </div>
         <div id="user-data" @click="changeMenuState" v-if="!isSmallScreen">
-          <UserData :user-surname="surname" :user-first-name="name" :user-image="image"/>
-          <font-awesome-icon :icon="menuIsShown ? iconChevronUp : iconChevronDown" id="arrow-icon"/>
+          <UserData :user-surname="surname" :user-first-name="name" :user-image="image" />
+          <font-awesome-icon :icon="menuIsShown ? iconChevronUp : iconChevronDown" id="arrow-icon" />
         </div>
       </div>
     </div>
     <div id="header-drawer">
       <img src="../../assets/logo.png" alt="Logo" @click="onLogoClick" class="logo">
-      <font-awesome-icon :icon="iconHamburger" id="hamburger-icon" class="showMenu" @click="changeMenuState"/>
+      <font-awesome-icon :icon="iconHamburger" id="hamburger-icon" class="showMenu" @click="changeMenuState" />
     </div>
   </div>
 </template>
 
 <script>
-import {COLORS, FONT_SIZES, GATEWAY_ADDRESS} from "../../../public/Consts";
+import { COLORS, FONT_SIZES, GATEWAY_ADDRESS } from "../../../public/Consts";
 import ButtonPrimary from "@/components/atoms/ButtonPrimary.vue";
 import UserData from "@/components/atoms/UserData.vue";
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-import {faBars, faChevronDown, faChevronUp} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faBars, faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Header",
-  components: {FontAwesomeIcon, UserData, ButtonPrimary},
+  components: { FontAwesomeIcon, UserData, ButtonPrimary },
   data() {
     return {
       COLORS: COLORS,
@@ -53,60 +53,60 @@ export default {
     }
   },
   methods:
-      {
-        onOffersClick() {
-          this.$router.push('/offers');
-        },
-        onMapClick() {
-          this.$router.push('/map');
-        },
-        onNewOfferClick() {
-          this.$router.push('/newOffer');
-        },
-        onAboutUsClick() {
-          this.$router.push('/about');
-        },
-        onLogoClick() {
-          this.$router.push("/");
-        },
-        changeMenuState() {
-          this.emitter.emit('change-menu');
-        },
-        async getUserData() {
-          let token = localStorage.getItem('token');
-          axios.get(GATEWAY_ADDRESS + `/user/get/${jwtDecode(token).id}`).then((response) => {
-            console.log('Logged User: ', response.data);
-            console.log('Logged image id: ', response.data.photoId.id);
-            this.name = response.data.name;
-            this.surname = response.data.surname;
-            this.photoId = response.data.photoId.id;
-            console.log('PhotoId: ', this.photoId);
-          })
-              .then(() => {
-                this.getImageData(this.photoId);
-              })
-              .catch(error => {
-                console.error('ERROR: ', error);
+  {
+    onOffersClick() {
+      this.$router.push('/offers');
+    },
+    onMapClick() {
+      this.$router.push('/map');
+    },
+    onNewOfferClick() {
+      this.$router.push('/newOffer');
+    },
+    onAboutUsClick() {
+      this.$router.push('/about');
+    },
+    onLogoClick() {
+      this.$router.push("/");
+    },
+    changeMenuState() {
+      this.emitter.emit('change-menu');
+    },
+    async getUserData() {
+      let token = localStorage.getItem('token');
+      axios.get(GATEWAY_ADDRESS + `/user/get/${jwtDecode(token).id}`).then((response) => {
+        console.log('Logged User: ', response.data);
+        console.log('Logged image id: ', response.data.photoId.id);
+        this.name = response.data.name;
+        this.surname = response.data.surname;
+        this.photoId = response.data.photoId.id;
+        console.log('PhotoId: ', this.photoId);
+      })
+        .then(() => {
+          this.getImageData(this.photoId);
+        })
+        .catch(error => {
+          console.error('ERROR: ', error);
 
-                this.emitter.emit('axiosError', {error: error.response.status});
-              });
-        },
-        async getImageData(photoId) {
-          console.log('PhotoId: ', photoId);
-          await axios.get(GATEWAY_ADDRESS + `/image/get/${photoId}`, {responseType: 'arraybuffer'}).then((response) => {
-            let image_buffer = this.arrayBufferToBase64(response.data);
-            this.image = `data:image/jpeg;base64,${image_buffer}`;
-          }).catch(error => {
-            console.error('ERROR: ', error);
-            this.emitter.emit('axiosError', {error: error.response.status});
-          });
-        },
-        arrayBufferToBase64(buffer) {
-          return btoa(
-              new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), '')
-          );
-        },
-      },
+          this.emitter.emit('axiosError', { error: error.response.status });
+        });
+    },
+    async getImageData(photoId) {
+      console.log('PhotoId: ', photoId);
+      await axios.get(GATEWAY_ADDRESS + `/image/get/${photoId}`, { responseType: 'arraybuffer' }).then((response) => {
+        let image_buffer = this.arrayBufferToBase64(response.data);
+        this.image = `data:image/jpeg;base64,${image_buffer}`;
+      }).catch(error => {
+        console.error('ERROR: ', error);
+        this.emitter.emit('axiosError', { error: error.response.status });
+      });
+    },
+    arrayBufferToBase64(buffer) {
+      return btoa(
+        new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), '')
+      );
+    },
+  },
   async mounted() {
     if (localStorage.getItem('token')) {
       await this.getUserData();
@@ -129,6 +129,13 @@ export default {
     width: 100%;
     height: 100px;
     background-color: v-bind('COLORS.PRIMARY');
+  }
+
+  #arrow-icon {
+    font-size: v-bind('FONT_SIZES.IMPORTANT');
+    color: v-bind('COLORS.TEXT_SECONDARY');
+    padding-left: 1em;
+    box-sizing: border-box;
   }
 
   #header_wrapper {
@@ -170,7 +177,8 @@ export default {
     margin-bottom: 0;
   }
 
-  #header-drawer img, .showMenu {
+  #header-drawer img,
+  .showMenu {
     display: none;
   }
 }

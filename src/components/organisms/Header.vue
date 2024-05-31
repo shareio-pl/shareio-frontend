7b5272a26d8c16169be1c172700b76d169c87e05
Rafal Cybula <242370@edu.p.lodz.pl>
2024-05-31 18:51:35 +0200

    Several bug fixes

--- src/components/organisms/Header.vue
+++ src/components/organisms/Header.vue
@@ -1,17 +1,21 @@
 <template>
-  <div id="header">
+  <div id="header" v-if="!showDrawer">
     <div id="header_wrapper">
-      <img src="../../assets/logo.png" @click="onLogoClick">
+      <img src="../../assets/logo.png" alt="" @click="onLogoClick" class="logo">
       <div id="buttons">
         <ButtonPrimary class="button" button-text="Ogłoszenia" @click="onOffersClick"/>
         <ButtonPrimary class="button" button-text="Mapa" @click="onMapClick"/>
         <ButtonPrimary class="button" button-text="Nowa oferta" @click="onNewOfferClick"/>
         <ButtonPrimary class="button" button-text="O nas" @click="onAboutUsClick"/>
       </div>
-      <div id="user-data" @click="changeMenuState">
-        <UserData :user-surname="this.surname" :user-first-name="this.name" :user-image="image"/>
+      <div id="user-data" @click="changeMenuState" v-if="!isSmallScreen">
+        <UserData :user-surname="this.surname" :user-first-name="this.name" :user-image="this.image"/>
         <font-awesome-icon :icon="menuIsShown ? iconChevronUp : iconChevronDown" id="arrow-icon"/>
       </div>
+    </div>
+    <div id="header-drawer">
+      <img src="../../assets/logo.png" alt="Logo" @click="onLogoClick" class="logo">
+      <font-awesome-icon :icon="iconHamburger" id="hamburger-icon" class="showMenu" @click="changeMenuState"/>
     </div>
   </div>
 </template>
@@ -21,9 +25,10 @@ import {COLORS, FONT_SIZES, GATEWAY_ADDRESS} from "../../../public/Consts";
 import ButtonPrimary from "@/components/atoms/ButtonPrimary.vue";
 import UserData from "@/components/atoms/UserData.vue";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-import {faChevronDown, faChevronUp} from "@fortawesome/free-solid-svg-icons";
+import {faBars, faChevronDown, faChevronUp} from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
-import { jwtDecode } from "jwt-decode";
+import {jwtDecode} from "jwt-decode";
+
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -33,14 +38,17 @@ export default {
     return {
       COLORS: COLORS,
       FONT_SIZES: FONT_SIZES,
+      iconHamburger: faBars,
       iconChevronDown: faChevronDown,
       iconChevronUp: faChevronUp,
       menuIsShown: false,
+
       surname: '',
       name: '',
       photoId: '',
       image: null,
-    };
+      isSmallScreen: false,
+    }
   },
   methods:
       {
@@ -70,16 +78,16 @@ export default {
             this.name = response.data.name;
             this.surname = response.data.surname;
             this.photoId = response.data.photoId.id;
-            console.log('PhotoId: ', this.photoId);          
+            console.log('PhotoId: ', this.photoId);
           })
-          .then(() => {
-            this.getImageData(this.photoId);
-          })
-          .catch(error => {
-            console.error('ERROR: ', error);
+              .then(() => {
+                this.getImageData(this.photoId);
+              })
+              .catch(error => {
+                console.error('ERROR: ', error);
 
-            this.emitter.emit('axiosError', {error: error.response.status});
-          });
+                this.emitter.emit('axiosError', {error: error.response.status});
+              });
         },
         async getImageData(photoId) {
           console.log('PhotoId: ', photoId);
@@ -113,53 +121,99 @@ export default {
 </script>
 
 <style scoped>
-#header {
-  display: flex;
-  width: 100%;
-  height: 100px;
-  background-color: v-bind('COLORS.PRIMARY');
+@media screen and (min-width: 850px) {
+  #header {
+    display: flex;
+    width: 100%;
+    height: 100px;
+    background-color: v-bind('COLORS.PRIMARY');
+  }
+
+  #header_wrapper {
+    display: flex;
+    width: 96%;
+    height: 100%;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 auto;
+  }
+
+  img {
+    cursor: pointer;
+    aspect-ratio: 3.166;
+    height: 100px;
+  }
+
+  .logo {
+    margin-left: 1%;
+  }
+
+  #buttons {
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    margin-left: 3%;
+  }
+
+  .button {
+    min-width: 50px;
+  }
+
+  #user-data {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    box-sizing: border-box;
+    margin-right: 1.1%;
+    margin-bottom: 0;
+  }
+
+  #header-drawer img, .showMenu {
+    display: none;
+  }
 }
 
-#header_wrapper {
-  display: flex;
-  width: 96%;
-  margin: 0 auto;
-  margin-right: 1%;
-  height: 100%;
-  align-items: center;
-  justify-content: space-between;
-}
-
-img {
-  cursor: pointer;
-  aspect-ratio: 3.166;
-  height: 100px;
-  /* 475 px / 150px */
-}
-
-#buttons {
-  flex-grow: 1;
-  display: flex;
-  align-items: center;
-  margin-left: 3%;
-}
-
-.button {
-  min-width: 150px;
-}
-
-#user-data {
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-  box-sizing: border-box;
-  margin-right: 1.1%;
-}
-
-#arrow-icon {
+#hamburger-icon {
   font-size: v-bind('FONT_SIZES.IMPORTANT');
   color: v-bind('COLORS.TEXT_SECONDARY');
-  padding-left: 1em;
-  box-sizing: border-box;
+  cursor: pointer;
+  margin-right: 11%;
+  z-index: 4;
+}
+
+@media screen and (max-width: 850px) {
+  #header {
+    display: none;
+  }
+
+  #header-drawer {
+    display: flex;
+    width: 100%;
+    height: 100px;
+    background-color: v-bind('COLORS.PRIMARY');
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 1%;
+    position: relative;
+  }
+
+  #header-drawer img {
+    cursor: pointer;
+    aspect-ratio: 3.166;
+    height: calc(3px + 11vw);
+  }
+
+  #header-drawer .showMenu {
+    position: absolute;
+    top: 50%;
+    right: 10px;
+    transform: translateY(-50%);
+    margin-right: 1%;
+  }
+
+  #hamburger-icon {
+    font-size: v-bind('FONT_SIZES.IMPORTANT');
+    color: v-bind('COLORS.TEXT_SECONDARY');
+  }
 }
 </style>
